9847733a81a8987820b94be571502ac66003f584
Hossam-C <chenaoui.hossam@gmail.com>
2020-03-09 14:49:07 +0100

    BugFix - TODO#2 -Implementation of the assertion in the integration test ParkingDataBaseIT :
       ==> Integration Test OK

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -1,9 +1,12 @@
 package com.parkit.parkingsystem.integration;
 
+import com.parkit.parkingsystem.constants.ParkingType;
 import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
 import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
+import com.parkit.parkingsystem.model.Ticket;
+import com.parkit.parkingsystem.service.FareCalculatorService;
 import com.parkit.parkingsystem.service.ParkingService;
 import com.parkit.parkingsystem.util.InputReaderUtil;
 import org.junit.jupiter.api.AfterAll;
@@ -13,7 +16,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.assertj.core.api.Assertions.*;
+
 
 @ExtendWith(MockitoExtension.class)
 public class ParkingDataBaseIT {
@@ -48,18 +55,43 @@ public class ParkingDataBaseIT {
     }
 
     @Test
-    public void testParkingACar(){
+    public void testParkingACar() throws Exception {
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processIncomingVehicle();
-        //TODO: check that a ticket is actualy saved in DB and Parking table is updated with availability
+        //Done : TODO: check that a ticket is actualy saved in DB and Parking table is updated with availability
+
+        Ticket ticketVerif;
+        ticketVerif = ticketDAO.getTicket(inputReaderUtil.readVehicleRegistrationNumber());
+
+        // Verification que le ticket du véhicule est bien enregistré
+        assertThat(ticketVerif.getId()).isEqualTo(1);
+        //Verification que la place de parking n'est plus disponible
+        assertThat(parkingSpotDAO.getNextAvailableSlot(ParkingType.CAR)).isGreaterThan(1);
     }
 
     @Test
-    public void testParkingLotExit(){
-        testParkingACar();
-        ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
-        parkingService.processExitingVehicle();
-        //TODO: check that the fare generated and out time are populated correctly in the database
+    public void testParkingLotExit() throws Exception {
+
+        ParkingService parkingServiceEntree = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+        parkingServiceEntree.processIncomingVehicle();
+        //Done : TODO: check that the fare generated and out time are populated correctly in the database
+
+        Thread.sleep(2000);
+
+
+        ParkingService parkingServiceSortie = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+        parkingServiceSortie.processExitingVehicle();
+        Ticket ticketVerifSortie = new Ticket();
+        ticketVerifSortie = ticketDAO.getTicket(inputReaderUtil.readVehicleRegistrationNumber());
+
+        //On verifie que l'on a toujours le ticket 1 enregistré
+        assertThat(ticketVerifSortie.getId()).isEqualTo(1);
+        // On verifie que la durée OutTime est bien insérée avec la bonne valeur
+        assertThat(ticketVerifSortie.getOutTime().getTime() - ticketVerifSortie.getInTime().getTime()).isEqualTo(2000);
+        // On vérifie que la place de parking correspondante est libre.
+        assertThat(parkingSpotDAO.getNextAvailableSlot(ParkingType.CAR)).isEqualTo(1);
+        //On vérifie que le montant est enregistré en base est bien celui voulu
+        assertThat(ticketVerifSortie.getPrice()).isEqualTo(0);
     }
 
 }
